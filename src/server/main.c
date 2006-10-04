3fc6ec3efebc927670b2eec3231288e4e2c9003e
Tomas Carnecky <tom@dbservice.com>
2006-10-04 20:55:46 +0000

    implement options (based on cubicools patch)

--- src/server/main.c
+++ src/server/main.c
@@ -24,19 +24,55 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+#include <getopt.h>
+#include <signal.h>
+
+static const int version[3] = { 0, 1, 0 };
+
+static char seomOutput[1024];
+static struct option seomServerOptions[] = {
+	{ "port",    1, 0, 'p' },
+	{ "outdir",  1, 0, 'o' },	
+	{ "version", 0, 0, 'v' },
+	{ "help",    0, 0, 'h' },
+	{ "daemon",  0, 0, 'd' },
+	{ 0, 0, 0, 0 }
+};
+
+static void sighandler(int signum)
+{
+	printf("Closing cleanly...\n");
+
+	exit(0);
+}
+
+static void help(void)
+{
+	printf(
+		"seomServer version %d.%d.%d\n"
+		"\t-p, --port:    port number\n"
+		"\t-o, --outdir:  output directory prefix\n"
+		"\t-v, --version: print version info\n"
+		"\t-h, --help:    print help text\n"
+		"\t-d, --daemon:  background server process; unimplimented!\n",
+		version[0], version[1], version[2]
+	);
+}
+
 static char *date(void)
 {
 	static char sbuf[32];
 	time_t tim = time(NULL);
-	struct tm *now = localtime(&tim);
-	snprintf(sbuf, 32, "%d-%02d-%02d--%02d:%02d:%02d", now->tm_year+1900, now->tm_mon+1, now->tm_mday, now->tm_hour, now->tm_min, now->tm_sec);
+	struct tm *tm = localtime(&tim);
+	snprintf(sbuf, 32, "%d-%02d-%02d--%02d:%02d:%02d",
+		tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
 	return sbuf;
 }
 
 static int output(char *name)
 {
 	char path[4096];
-	snprintf(path, 4096, "%s/seom/%s.seom", getenv("HOME"), name);
+	snprintf(path, 4096, "%s/%s.seom", seomOutput, name);
 	return open(path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IRGRP | S_IROTH);
 }
 
@@ -52,7 +88,7 @@ static void *loop(void *arg)
 	
 	pthread_mutex_lock(&mutex);
 	char *now = date();
-	printf("[%s]: >>> %d\n", now, data - threads);
+	printf("[%s]: >>> %d\n", now, (int) (data - threads));
 	int file = output(now);
 	pthread_mutex_unlock(&mutex);
 	
@@ -99,7 +135,7 @@ out:
 	pthread_mutex_lock(&mutex);
 	data->socket = -1;
 	now = date();
-	printf("[%s]: <<< %d\n", now, data - threads);
+	printf("[%s]: <<< %d\n", now, (int) (data - threads));
 	pthread_mutex_unlock(&mutex);
 	
 	return NULL;
@@ -123,19 +159,49 @@ out:
 }
 
 int main(int argc, char *argv[])
-{	
-	if (argc == 1) {
-		fprintf(stderr, "Usage: %s [port]\n", argv[0]);
-		return 0;
-	}
+{
+	int opindex = 0;
+	int port = 9000;
+	
+	snprintf(seomOutput, sizeof(seomOutput), "%s/seom", getenv("HOME"));
+
+	while(1)  {
+		int c = getopt_long(argc, argv, "p:o:vhd", seomServerOptions, &opindex);
+		
+		if (c == -1) {
+			break;
+		} else {
+			switch(c) {
+			case 'p':
+				port = atoi(optarg);
+				break;
+			case 'o':
+				snprintf(seomOutput, sizeof(seomOutput), "%s", optarg);
+				break;
+			case 'v':
+				printf("%d.%d.%d\n", version[0], version[1], version[2]);
+				exit(0);
+			case 'h':
+				help();
+				exit(0);
+			case 'd':
+				printf("Daemonizing unimplimented; please check back.\n");
+				break;
+			default:
+				break;
+			}
+		}
+ 	}
+ 	
+	printf("seomServer running on port %d using output directory %s\n", port, seomOutput);
+
+	signal(SIGINT, sighandler);
 	
 	pthread_mutex_init(&mutex, NULL);
 	
 	for (int i = 0; i < 16; ++i) {
 		threads[i].socket = -1;
 	}
-	
-	int port = atoi(argv[1]);
 		
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	
