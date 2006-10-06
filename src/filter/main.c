1e4c8a3f6b18d7d45fa6610d7de8fc3ad62ade34
Tomas Carnecky <tom@dbservice.com>
2006-10-06 22:19:35 +0000

    fix filter, now creates valid y4m files but doesn't correct the framerate.

--- src/filter/main.c
+++ src/filter/main.c
@@ -44,33 +44,27 @@ int main(int argc, char *argv[])
 		}
 	}
 	
-	uint32_t size[2];
-	read(0, size, sizeof(size));
+	/* FIXME */
+	close(0);
+	open(argv[1], O_RDONLY);
 	
-	size[0] = ntohl(size[0]);
-	size[1] = ntohl(size[1]);
+	struct { uint32_t x; uint32_t y; } size[3];
+	read(0, &size[0].x, sizeof(size[0].x));
+	read(0, &size[0].y, sizeof(size[0].y));
 	
-	uint8_t *yuvPlanes[3];
-	struct { uint64_t x; uint64_t y; } yuvPlanesSizes[3];
+	size[0].x = ntohl(size[0].x);
+	size[0].y = ntohl(size[0].y);
 	
-	yuvPlanesSizes[0].x = size[0];
-	yuvPlanesSizes[0].y = size[1];
+	size[1].x = size[2].x = size[0].x / 2;
+	size[1].y = size[2].y = size[0].y / 2;
 	
-	yuvPlanesSizes[1].x = size[0] / 2;
-	yuvPlanesSizes[1].y = size[1] / 2;
-	
-	yuvPlanesSizes[2].x = size[0] / 2;
-	yuvPlanesSizes[2].y = size[1] / 2;
-	
-	yuvPlanes[0] = malloc(size[0] * size[1] * 3 / 2);
-	yuvPlanes[1] = yuvPlanes[0] + yuvPlanesSizes[0].x * yuvPlanesSizes[0].y;
-	yuvPlanes[2] = yuvPlanes[1] + yuvPlanesSizes[1].x * yuvPlanesSizes[1].y;
-	
-	fprintf(stderr, "size: %d:%d\n", size[0], size[1]);
+	uint64_t len = size[0].x * size[0].y;
+	uint32_t *tmp = malloc(len);
+	uint8_t *data = malloc(size[0].x * size[0].y * 3 / 2);
 	
 	char header[4096];
-	int n = snprintf(header, 4096, "YUV4MPEG2 W%d H%d F%d Ip\n", size[0], size[1], fps);
-	write(1, header, n);
+	int n = snprintf(header, 4096, "YUV4MPEG2 W%d H%d F%d Ip\n", size[0].x, size[0].y, fps);
+	write(1, header, strlen(header));
 	
 	for (;;) {
 		uint64_t pts;
@@ -78,33 +72,32 @@ int main(int argc, char *argv[])
 			break;
 		
 		n = snprintf(header, 4096, "FRAME\n");
-		write(1, header, n);
+		write(1, header, strlen(header));
 		
 		for (int i = 0; i < 3; ++i) {
 			uint64_t length;
 			read(0, &length, sizeof(length));
-			uint32_t *tmp = malloc(length);
+			if (length > len) {
+				len = length;
+				tmp = realloc(tmp, len);
+			}
 			read(0, tmp, length);
-			seomCodecDecode(yuvPlanes[i], tmp, yuvPlanes[i] + yuvPlanesSizes[i].x * yuvPlanesSizes[i].y, &seomCodecTable);
-			free(tmp);
+			seomCodecDecode(data, tmp, data + size[i].x * size[i].y, &seomCodecTable);
 			
-			uint64_t width = yuvPlanesSizes[i].x;
-			uint64_t height = yuvPlanesSizes[i].y;
-			
-#define src(x,y) ( yuvPlanes[i][(y)*width+(x)] )
-			for (unsigned int x = 1; x < width; ++x) {
-				yuvPlanes[i][x] += median(src(x-1,0), 0, 0);
+#define src(xo,yo) ( data[(yo) * size[i].x + (xo)] )
+			for (uint32_t x = 1; x < size[i].x; ++x) {
+				data[x] += median(src(x-1,0), 0, 0);
 			}
 			
-			for (unsigned int y = 1; y < height; ++y) {
-				yuvPlanes[i][y * width] += median(0, 0, src(0,y-1));
-				for (unsigned int x = 1; x < width; ++x) {
-					yuvPlanes[i][y * width + x] =+ median(src(x-1,y), src(x-1,y-1), src(x,y-1));
+			for (uint32_t y = 1; y < size[i].y; ++y) {
+				data[y * size[i].x] += median(0, 0, src(0,y-1));
+				for (uint32_t x = 1; x < size[i].x; ++x) {
+					data[y * size[i].x + x] += median(src(x-1,y), src(x-1,y-1), src(x,y-1));
 				}
 			}
 			
-			for (unsigned int y = height - 1; y < height; --y) {   
-				write(1, yuvPlanes[i] + y * width, width);
+			for (uint32_t y = size[i].y - 1; y < size[i].y; --y) { 
+				write(1, data + y * size[i].x, size[i].x);
 			}
 #undef src
 		}
