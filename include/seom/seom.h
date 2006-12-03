4f6b72bd372b68f996cbb4ac505c91fc962fa37d
Tomas Carnecky <tom@dbservice.com>
2006-12-03 22:38:22 +0000

    clean up header

--- include/seom/seom.h
+++ include/seom/seom.h
@@ -8,32 +8,30 @@
 #define _GNU_SOURCE
 #define _BSD_SOURCE
 
-#include <errno.h>
 #include <assert.h>
+#include <errno.h>
 
+#include <sys/mman.h>
+#include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
-#include <sys/socket.h>
 #include <sys/stat.h>
-#include <sys/mman.h>
 
-#include <pthread.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <dlfcn.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 #include <GL/gl.h>
-#include <GL/glext.h>
-#include <GL/glx.h>
 
 #include <X11/Xlib.h>
-#include <X11/Xatom.h>
+#include <X11/Xutil.h>
 #include <X11/keysym.h>
 
 #include <seom/config.h>
@@ -43,12 +41,6 @@
 #include <seom/stream.h>
 #include <seom/client.h>
 
-/*
-typedef seomTime uint64_t
-#define seomTimeCurrent() ({ struct timeval tv; gettimeofday(&tv, 0); (seomTime) tv.tv_sec * 1000000 + tv.tv_usec; })
-*/
-
 #define seomTime() ({ struct timeval tv; gettimeofday(&tv, 0); (uint64_t) tv.tv_sec * 1000000 + tv.tv_usec; })
-	
 
 #endif /* __SEOM_H__ */
